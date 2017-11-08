aeb6bbc3429759106e1a38ccef9c36b90973a51c
thetallahassee <enric.velasco.g@gmail.com>
2017-11-08 18:26:56 +0100

    Initial commit

---
+++ src/main/java/com/example/MavenServidor/MavenServidorApplication.java
@@ -0,0 +1,14 @@
+package com.example.MavenServidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenServidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenServidorApplication.class, args);
+
+
+	}
+}
---
+++ src/test/java/com/example/MavenServidor/MavenServidorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.MavenServidor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MavenServidorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
