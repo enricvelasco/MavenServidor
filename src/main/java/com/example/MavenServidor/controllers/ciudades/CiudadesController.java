36fe144678717d9e52455f7af030f2e936912a52
enric <enric.velasco.g@gmail.com>
2017-12-21 23:30:20 +0100

    query en GET All v2

---
+++ src/main/java/com/example/MavenServidor/controllers/ciudades/CiudadesController.java
@@ -0,0 +1,48 @@
+package com.example.MavenServidor.controllers.ciudades;
+
+import com.example.MavenServidor.controllers.RESTFulController;
+import com.example.MavenServidor.services.CiudadesService;
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ciudades")
+public class CiudadesController extends RESTFulController {
+    private CiudadesService ciudadesService;
+    public CiudadesController(){
+        dominio = "Tciudades";
+    }
+
+    @Override
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public JSONArray list(@RequestParam(value = "query") String query) {
+        return super.list(query);
+    }
+
+    @Override
+    @RequestMapping( value = "/{id}", method = RequestMethod.GET )
+    @ResponseBody
+    public JSONObject read(@PathVariable("id") String id) {
+        return super.read(id);
+    }
+
+    @Override
+    @RequestMapping( value = "/", method = RequestMethod.POST )
+    public JSONObject create(@RequestBody String jsonObject) {
+        return super.create(jsonObject);
+    }
+
+    @Override
+    @RequestMapping( value = "/{id}", method = RequestMethod.PUT )
+    public JSONObject update(@RequestBody String jsonObject) {
+        return super.update(jsonObject);
+    }
+
+    @Override
+    @RequestMapping( value = "/", method = RequestMethod.DELETE )
+    public String delete(@RequestBody String jsonObject) {
+        return super.delete(jsonObject);
+    }
+}
